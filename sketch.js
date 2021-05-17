4659a419727f9604a20bb677cb4787dd5c006dac
MrKevlar28 <77254796+MrKevlar28@users.noreply.github.com>
2021-05-17 15:48:30 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,121 @@
+var backImage,backgr;
+var player, player_running;
+var ground, ground_img;
+var bananaImage, obstacleImage;
+var gameOverImage;
+
+var END = 0;
+var PLAY = 1;
+var gameState = PLAY;
+var score = 0;
+var gameOver
+
+
+function preload(){
+  backImage = loadImage("jungle.jpg");
+  player_running = loadAnimation("Monkey_01.png","Monkey_02.png","Monkey_03.png","Monkey_04.png","Monkey_05.png","Monkey_06.png","Monkey_07.png","Monkey_08.png","Monkey_09.png","Monkey_10.png");
+  bananaImage = loadImage("banana.png");
+  obstacleImage = loadImage("stone.png");
+  gameOverImage = loadImage("gameOver.png");
+}
+
+function setup() {
+  createCanvas(800,400);
+  
+  backgr = createSprite(0,0,800,400);
+  backgr.addImage(backImage);
+  backgr.scale=1.5;
+  backgr.x=backgr.width/2;
+  backgr.velocityX=-4;
+  
+  player = createSprite(100,340,20,50);
+  player.addAnimation("Running",player_running);
+  player.scale = 0.1;
+  
+  ground = createSprite(400,350,800,10);
+  ground.x=ground.width/2;
+  ground.visible = false;
+
+  gameOver = createSprite(400,220,60,50);
+  gameOver.addImage(gameOverImage);
+  gameOver.scale = 0.8;
+  
+  FoodGroup = createGroup();
+  ObstaclesGroup = createGroup();
+}
+
+function draw() { 
+  background(0);
+  drawSprites();
+
+  if(gameState===PLAY){
+  
+  if(backgr.x<100){
+    backgr.x=backgr.width/2;
+  }
+   
+  gameOver.visible = false;
+
+   spawnFood();
+   spawnObstacles();
+
+    if(keyWentDown("space") ) {
+      player.velocityY = -12;
+    }
+    player.velocityY = player.velocityY + 0.8;
+  
+    player.collide(ground);
+
+    if(FoodGroup.isTouching(player)){
+      FoodGroup.destroyEach();
+      score = score + 2;
+      player.scale = player.scale + 0.1
+    }
+    if(ObstaclesGroup.isTouching(player)){
+      gameState = END;
+    }
+
+  }
+  if(gameState === END){
+    backgr.velocityX = 0;
+    player.visible = false;
+
+    FoodGroup.destroyEach();
+    ObstaclesGroup.destroyEach();
+
+    gameOver.visible = true;
+
+    // textSize(30);
+    // fill(255);
+    // text("GAME OVER!", 300,220);
+
+  }
+  
+  fill("white");
+  textSize(25);
+  text("Score : " + score, 600,100);
+}
+
+function spawnFood(){
+  if(frameCount % 80 === 0) {
+    var banana = createSprite(600,250,40,10);
+    banana.y = random(120,200);
+    banana.addImage(bananaImage);
+    banana.scale = 0.05;
+    banana.velocityX = -4;
+    banana.lifetime = 300;
+    player.depth = banana.depth + 1;
+    FoodGroup.add(banana);
+  }
+}
+
+function spawnObstacles(){
+  if(frameCount % 60 === 0){
+    var obstacle = createSprite(600,340,40,10);
+    obstacle.addImage(obstacleImage);
+    obstacle.scale = 0.2;
+    obstacle.velocityX = -5;
+    obstacle.lifetime = 300;
+    ObstaclesGroup.add(obstacle);
+  }
+}
